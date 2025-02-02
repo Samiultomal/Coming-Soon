326ea1c4cc1ff16af57cf94a9de498a9bec0aeb4
Samiul Tomal <tomalsarker26@gmail.com>
2025-02-02 11:12:48 +0600

    Updated settings.py with new static/media paths and security settings

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -86,13 +86,25 @@ USE_I18N = True
 USE_TZ = True
 
 
+# # Static files (CSS, JavaScript, Images)
+# STATIC_URL = '/static/'
+# STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
+# # Media files (User-uploaded files)
+# MEDIA_URL = '/media/'
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+
 # Static files (CSS, JavaScript, Images)
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
+STATIC_ROOT = '/var/www/lbackpage/backend/static/'
 
 # Media files (User-uploaded files)
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = '/var/www/lbackpage/backend/media/'
 
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
